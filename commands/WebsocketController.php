29ada961880c89dcce88d1dac915fa405018520a
Arduanov Danil <rylezz@gmail.com>
2025-08-09 14:40:11 +0500

    Add websocket

--- commands/WebsocketController.php
+++ commands/WebsocketController.php
@@ -4,19 +4,66 @@
 namespace app\commands;
 
 use yii\console\Controller;
-use yii\console\ExitCode;
 
+use Amp\Http\Server\DefaultErrorHandler;
+use Amp\Http\Server\Router;
+use Amp\Http\Server\SocketHttpServer;
+use Amp\Http\Server\StaticContent\DocumentRoot;
+use Amp\Log\ConsoleFormatter;
+use Amp\Log\StreamHandler;
+use Amp\Socket;
+use Amp\Websocket\Server\AllowOriginAcceptor;
+use Amp\Websocket\Server\Websocket;
+use app\websocket\Handler;
+use Monolog\Logger;
+use function Amp\trapSignal;
+use function Amp\ByteStream\getStdout;
 
 class WebsocketController extends Controller
 {
     /**
-     * @param string $message the message to be echoed.
      * @return int Exit code
      */
-    public function actionStart($message = 'hello world')
+    public function actionStart()
     {
-        echo $message . "\n";
+        //var_dump(111);
 
-        return ExitCode::OK;
+        $logHandler = new StreamHandler(getStdout());
+        $logHandler->setFormatter(new ConsoleFormatter());
+        $logger = new Logger('server');
+        $logger->pushHandler($logHandler);
+
+        $server = SocketHttpServer::createForDirectAccess($logger);
+
+        $server->expose(new Socket\InternetAddress('127.0.0.1', 1337));
+        $server->expose(new Socket\InternetAddress('[::1]', 1337));
+
+        $errorHandler = new DefaultErrorHandler();
+
+        $acceptor = new AllowOriginAcceptor(
+            ['http://localhost:1337', 'http://127.0.0.1:1337', 'http://[::1]:1337'],
+        );
+
+        $clientHandler = new Handler($logger);
+
+        $websocket = new Websocket($server, $logger, $acceptor, $clientHandler);
+    
+        $router = new Router($server, $logger, $errorHandler);
+        $router->addRoute('GET', '/broadcast', $websocket);
+        $path = __DIR__ . '/../public';
+
+
+        $router->setFallback(new DocumentRoot($server, $errorHandler, __DIR__ . '/public'));
+
+        $server->start($router, $errorHandler);
+
+        // Await SIGINT or SIGTERM to be received.
+        $signal = trapSignal([SIGINT, SIGTERM]);
+
+        $logger->info(sprintf("Received signal %d, stopping HTTP server", $signal));
+
+
+        $server->stop();
+
     }
 }
---
+++ websocket/Handler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\websocket;
+
+use Amp\Http\Server\Request;
+use Amp\Http\Server\Response;
+use Amp\Websocket\Server\WebsocketClientHandler;
+use Amp\Websocket\Server\WebsocketGateway;
+use Amp\Websocket\Server\WebsocketClientGateway;
+use Amp\Websocket\WebsocketClient;
+use Monolog\Logger;
+use function Amp\Redis\createRedisClient;
+
+class Handler implements WebsocketClientHandler
+{
+
+    public function __construct(private readonly Logger $logger, private readonly WebsocketGateway $gateway = new WebsocketClientGateway())
+    {
+        //var_dump(1000);
+
+    }
+
+
+    public function handleClient(
+                WebsocketClient $client,
+                Request $request,
+                Response $response,
+            ): void {
+                $this->logger->info('Message from handler');
+
+                $authHeader = $request->getHeader('Token');
+
+                $authHeader = $authHeader ?? 'test';
+
+                $this->logger->info($authHeader);
+
+                $this->gateway->addClient($client);
+
+                
+                
+                foreach ($client as $message) {
+
+                    $this->gateway->sendText('text for send', $client->getId());
+
+                    $this->gateway->broadcastText(sprintf(
+                        '%d: %s',
+                        $client->getId(),
+                        (string) $message,
+                    ));
+                }
+            }
+}
